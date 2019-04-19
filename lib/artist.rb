3cdb0b72752f07723aab1c419304a78796a12118
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-19 01:51:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,7 @@
 class Artist
-  attr_accessor :artist, :name
+  attr_accessor :name
   
-  def initialize(name, artist)
+  def initialize(name)
     @name = name
-    @artist = artist
   end
 end
-
-artist = Artist.new("Jean-Kenel", "Player")
